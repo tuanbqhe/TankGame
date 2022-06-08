2c5a0a0ea551ef9b87a2bfe6b379ff7845dd8716
tuanbqhe <tuanbqhe141256@gmail.com>
2022-06-08 23:43:27 +0700

    d

--- test.js
+++ test.js
@@ -4,26 +4,3 @@
           this.age = 13;
       }
 }
-class student extends person {
-    constructor() {
-        super();
-        this.grade = 2;
-    }
-    print () {
-        console.log("hello");
-    }
-}
-
-<<<<<<< HEAD
-=======
-const stu = new student();
-const per = new person();
-let k = stu;
-stu.age = 100;
-console.log(k);
-11111dwrerw
-11111
-fewafaeads
-new 2
-new 2
->>>>>>> 88dd091589dff90541ae18c22f92fb9b88d472ca
